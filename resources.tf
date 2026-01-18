f9c0edc7ae5ba963edc3e6b90ae6803e55e63da4
Aves4 <160195975+Aves4@users.noreply.github.com>
2026-01-18 14:30:22 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -74,7 +74,7 @@
     name                = "A-sshk"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
-    public_key          = file("~/.ssh/id_rsa.pub")
+    public_key          = var.ssh_public_key
     }
 
     resource "azurerm_linux_virtual_machine" "vmtf" {
